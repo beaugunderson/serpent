093c5ce90247f020720b33ece1754cbe47e7b117
Beau Gunderson <beau@beaugunderson.com>
2017-07-19 00:13:17 -0700

    be smarter

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,9 +4,10 @@ set -e -x
 
 yum install -y openssl openssl-devel
 
+PYTHON_VERSIONS=$( /opt/python/cp27-cp27m/bin /opt/python/cp27-cp27mu/bin /opt/python/cp36-cp36m/bin )
+
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install twine
+for PYBIN in $PYTHON_VERSIONS; do
     "${PYBIN}/pip" install -r /io/requirements-dev.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -17,8 +18,10 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in $PYTHON_VERSIONS; do
     "${PYBIN}/pip" install ethereum-serpent-augur-temp --no-index -f /io/wheelhouse
     # (cd /io; "${PYBIN}/py.test")
-    "${PYBIN}/twine" upload --skip-existing -u beaugunderson -p "$PYPI_PASSWORD" wheelhouse/*
 done
+
+/opt/python/cp36-cp36m/bin/pip install twine
+/opt/python/cp36-cp36m/bin/twine upload --skip-existing -u beaugunderson -p "$PYPI_PASSWORD" wheelhouse/*
